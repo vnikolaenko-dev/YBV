bf447b84bfb03b70112f9cc9cbb224ca49e626e1
Victor Nikolaenko <viktor.nikolaenko.2005@gmail.com>
2025-04-14 19:46:28 +0300

    update security

--- src/main/java/org/example/backend/model/response/StatusResponse.java
+++ src/main/java/org/example/backend/model/response/StatusResponse.java
@@ -1,4 +1,13 @@
 package org.example.backend.model.response;
 
+import lombok.Data;
+import org.example.backend.model.enums.Status;
+
+@Data
 public class StatusResponse {
+    private Status status;
+
+    public StatusResponse(Status status) {
+        this.status = status;
+    }
 }
