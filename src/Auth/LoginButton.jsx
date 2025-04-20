1d427a7e9ba2cea021e70ab5417b86791498cbdb
Tralalelo <nabokova_alisa2005@mail.ru>
2025-04-21 00:57:26 +0300

    rebase

--- src/Auth/LoginButton.jsx
+++
@@ -1,9 +0,0 @@
-import {useNavigate} from 'react-router-dom'
-function LoginButton(){
-    const navigate = useNavigate();
-
-    return(
-        <button className="login-button" onClick={() => navigate("/auth")}>Become better</button>
-    )
-}
-export default LoginButton;
---
+++ src/habit/blockes/SmallSquare.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const SmallSquare = styled.div`
+    position: absolute;
+    height: 267px;
+    width: 267px;
+    border-radius: 20%;
+    background-color: #1C1C1E
+`;
