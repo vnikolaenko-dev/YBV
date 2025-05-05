eb4a4e3c00cf93e4374280e12fcb5558f5e44721
Victor Nikolaenko <64604542+vnikolaenko-dev@users.noreply.github.com>
2025-05-05 19:47:21 +0300

    Delete src directory

--- src/habit/blockes/SmallSquare.jsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const SmallSquare = styled.div`
-    position: absolute;
-    height: 267px;
-    width: 267px;
-    border-radius: 20%;
-    background-color: #1C1C1E
-`;
