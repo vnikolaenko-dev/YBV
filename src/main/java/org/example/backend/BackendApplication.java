71f4f2e45959800e07a27d34653c082c52578afc
Victor Nikolaenko <viktor.nikolaenko.2005@gmail.com>
2025-05-24 23:04:42 +0300

    update

--- src/main/java/org/example/backend/BackendApplication.java
+++ src/main/java/org/example/backend/BackendApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "org.example.backend.repository")
 @EntityScan(basePackages = "org.example.backend.model.entity")
+// @EnableScheduling
 public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
