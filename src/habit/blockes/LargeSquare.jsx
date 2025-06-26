8173f0112a199e4b21edd47081a14a0b1c35bda9
Victor Nikolaenko <viktor.nikolaenko.2005@gmail.com>
2025-06-26 20:26:00 +0300

    local test version

---
+++ src/habit/blockes/LargeSquare.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const LargeSquare = styled.div`
+    position: absolute;
+    height: 557px;
+    width: 557px;
+    border-radius: 20%;
+    background-color: #1C1C1E
+`;
